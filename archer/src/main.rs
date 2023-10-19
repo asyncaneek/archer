1d81db4c67060cfc4b3d93bf29a7ec3ea861acbb
asyncaneek <45776865+asyncaneek@users.noreply.github.com>
2023-10-19 16:10:39 +0000

    pipe command output to TextView

--- archer/src/main.rs
+++ archer/src/main.rs
@@ -1,13 +1,16 @@
 use cursive::view::Nameable;
 use cursive::views::{TextContent, TextView};
 use cursive::{Cursive, CursiveExt, View};
+
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::process::{Command, Stdio};
 use std::thread;
 
 fn main() {
     let mut siv = Cursive::default();
     siv.add_global_callback('q', |s| s.quit());
 
-    siv.add_layer(make_content_area() );
+    siv.add_layer(make_content_area());
 
     // Start the Cursive UI event loop
     siv.set_autorefresh(true);
@@ -25,12 +28,27 @@ fn make_content_area() -> Box<dyn View> {
     thread::spawn(move || {
         // Simulate some asynchronous process
         thread::sleep(std::time::Duration::from_secs(3));
-
         // Update the text in the shared state
-        thread_shared_text.append("\nYeet! It worked 🖖");
+        let _ = run_command(thread_shared_text.clone());
+        // thread_shared_text.append("\nYeet! It worked 🖖");
     });
 
     // Create a TextView and add it to the Cursive UI
     let text_view = TextView::new_with_content(content.clone()).with_name("content_area");
     Box::new(text_view)
+}
+
+fn run_command(text_content: TextContent) -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = Command::new("ls")
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Could not capture standard output."))?;
+
+    let reader = BufReader::new(stdout);
+    reader.lines().for_each(move |line| {
+        text_content.append("\n".to_string());
+        text_content.append(line.unwrap_or("-".to_string()));
+    });
+    Ok(())
 }
